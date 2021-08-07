a04cac03d27132c0ff04916b1376260a52f70ef1
Saurabh-ux-92199 <sanjayhhood@gmail.com>
2021-08-07 19:32:22 +0530

    edit

--- App.js
+++ App.js
@@ -72,7 +72,7 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center', backgroundColor: 'skyblue',
+    justifyContent: 'center', backgroundColor: 'blue',
   },
   main: { 
     justifyContent: 'center',
